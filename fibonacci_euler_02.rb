27b275cce67da75b0d79b2cd44c694d06afccd99
Susan Prestage <susan.prestage@gmail.com>
2014-05-11 17:47:47 -0700

    Give even fibonacci numbers program friendlier user output.

--- fibonacci_euler_02.rb
+++ fibonacci_euler_02.rb
@@ -5,12 +5,14 @@ fib_n_minus_two = 1
 fib_n_minus_one = 1
 fib_n = 0
 
+puts
+
 until fib_n > 4_000_000
   fib_n = fib_n_minus_one + fib_n_minus_two
   fib_n_minus_two = fib_n_minus_one
   fib_n_minus_one = fib_n
   if fib_n_minus_two % 2 == 0
-    # print "#{fib_n_minus_two} "
+    print "#{fib_n_minus_two} "
     fib_sum += fib_n_minus_two
   end
 end
