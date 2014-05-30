c9d5e2e8a1260e4a050255869a979dec6b374bc2
Susan Prestage <susan.prestage@gmail.com>
2014-05-30 15:02:18 -0700

    Refactor Euler 01, Sum of multiples of 3 or 5.

--- multiples_of_3_or_5_euler_01.rb
+++ multiples_of_3_or_5_euler_01.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/env ruby
+###
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#
+# Find the sum of all the multiples of 3 or 5 below 1000.
+###
 
-target = 1000
-sum = 0
+TARGET = 1000
 
 def isDivisibleByThree(number)
   return true if number % 3 == 0
@@ -11,13 +15,17 @@ def isDivisibleByFive(number)
   return true if number % 5 == 0
 end
 
-1.upto(target-1) do |x|
-  # puts x
-  if ((isDivisibleByFive(x)) || (isDivisibleByThree(x)))
+def sumOfMultiples(number)
+  sum = 0
+  1.upto(number-1) do |x|
     # puts x
-    sum = sum + x
+    if ((isDivisibleByFive(x)) || (isDivisibleByThree(x)))
+      # puts x
+      sum = sum + x
+    end
   end
+  return sum
 end
 
-puts "sum = #{sum}"
+puts "sum = #{sumOfMultiples(TARGET)}"
 
