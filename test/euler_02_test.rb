0df47e52758f873579143cbc536b58500dd8e512
Susan Prestage <susan.prestage@gmail.com>
2014-06-04 13:51:38 -0700

    Add test for successful answer to Euler #2 challenge.

--- test/euler_02_test.rb
+++ test/euler_02_test.rb
@@ -3,27 +3,27 @@ require_relative '../fibonacci_euler_02.rb'
 
 class TestFibonacci < MiniTest::Test
   def test_initial_fibonacci
-    assert_equal 0, findXthFibonacciNumber(0)
+    assert_equal 0, find_xth_fibonacci_number(0)
   end
 
   def test_first_fibonacci
-    assert_equal 1, findXthFibonacciNumber(1)
+    assert_equal 1, find_xth_fibonacci_number(1)
   end
 
   def test_second_fibonacci
-    assert_equal 1, findXthFibonacciNumber(2)
+    assert_equal 1, find_xth_fibonacci_number(2)
   end
 
   def test_third_fibonacci
-    assert_equal 2, findXthFibonacciNumber(3)
+    assert_equal 2, find_xth_fibonacci_number(3)
   end
 
   def test_fourth_fibonacci
-    assert_equal 3, findXthFibonacciNumber(4)
+    assert_equal 3, find_xth_fibonacci_number(4)
   end
 
   def test_fifth_fibonacci
-    assert_equal 5, findXthFibonacciNumber(5)
+    assert_equal 5, find_xth_fibonacci_number(5)
   end
 end
 
@@ -54,3 +54,28 @@ class TestFib < MiniTest::Test
   end
 end
 
+class TestSolutions < MiniTest::Test
+  def test_first_solution_with_small_example_answer
+    assert_equal 10, first_find_sum_of_even_fibonacci_terms_under(10)
+  end
+
+  def test_second_solution_small_example_euler_answer
+    assert_equal 10, second_find_sum_of_even_fibonacci_terms_under(10)
+  end
+
+  def test_third_solution_small_example_euler_answer
+    assert_equal 10, third_find_sum_of_even_fibonacci_terms_under(10)
+  end
+
+  def test_first_solution_euler_challenge
+    assert_equal 4613732, first_find_sum_of_even_fibonacci_terms_under(4_000_000)
+  end
+
+  def test_second_solution_euler_challenge
+    assert_equal 4613732, second_find_sum_of_even_fibonacci_terms_under(4_000_000)
+  end
+
+  def test_third_solution_euler_challenge
+    assert_equal 4613732, third_find_sum_of_even_fibonacci_terms_under(4_000_000)
+  end
+end
