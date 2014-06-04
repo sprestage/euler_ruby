5f9ec16fb70a9412c91c6d64dc20055abecd6484
Susan Prestage <susan.prestage@gmail.com>
2014-06-04 14:15:58 -0700

    Correct methods to use snake case according to standard ruby style.

--- multiples_of_3_or_5_euler_01.rb
+++ multiples_of_3_or_5_euler_01.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 ###
+# Multiples of 3 and 5
+# Problem 1
+###
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #
 # Find the sum of all the multiples of 3 or 5 below 1000.
@@ -7,19 +10,19 @@
 
 TARGET = 1000
 
-def isDivisibleByThree(number)
+def is_divisible_by_three(number)
   return true if number % 3 == 0
 end
 
-def isDivisibleByFive(number)
+def is_divisible_by_five(number)
   return true if number % 5 == 0
 end
 
-def sumOfMultiples(number)
+def sum_of_multiples(number)
   sum = 0
   1.upto(number-1) do |x|
     # puts x
-    if ((isDivisibleByFive(x)) || (isDivisibleByThree(x)))
+    if ((is_divisible_by_five(x)) || (is_divisible_by_three(x)))
       # puts x
       sum = sum + x
     end
@@ -27,5 +30,5 @@ def sumOfMultiples(number)
   return sum
 end
 
-puts "sum = #{sumOfMultiples(TARGET)}"
+puts "sum = #{sum_of_multiples(TARGET)}"
 
--- test/euler_01_test.rb
+++ test/euler_01_test.rb
@@ -3,73 +3,73 @@ require_relative '../multiples_of_3_or_5_euler_01.rb'
 
 class TestDivisibleByThree < MiniTest::Test
   def test_is_zero_divisible_by_three
-    assert_equal true, isDivisibleByThree(0)
+    assert_equal true, is_divisible_by_three(0)
   end
 
   def test_is_one_divisible_by_three
-    assert_equal true, !isDivisibleByThree(1)
+    assert_equal true, !is_divisible_by_three(1)
   end
 
   def test_is_two_divisible_by_three
-    assert_equal true, !isDivisibleByThree(2)
+    assert_equal true, !is_divisible_by_three(2)
   end
 
   def test_is_three_divisible_by_three
-    assert_equal true, isDivisibleByThree(3)
+    assert_equal true, is_divisible_by_three(3)
   end
 
   def test_is_five_divisible_by_three
-    assert_equal true, !isDivisibleByThree(5)
+    assert_equal true, !is_divisible_by_three(5)
   end
 
   def test_is_square_of_three_divisible_by_three
-    assert_equal true, isDivisibleByThree(9)
+    assert_equal true, is_divisible_by_three(9)
   end
 
   def test_is_fifteen_divisible_by_three
-    assert_equal true, isDivisibleByThree(15)
+    assert_equal true, is_divisible_by_three(15)
   end
 end
 
 
 class TestDivisibleByFive < MiniTest::Test
   def test_is_zero_divisible_by_five
-    assert_equal true, isDivisibleByFive(0)
+    assert_equal true, is_divisible_by_five(0)
   end
 
   def test_is_one_divisible_by_five
-    assert_equal true, !isDivisibleByFive(1)
+    assert_equal true, !is_divisible_by_five(1)
   end
 
   def test_is_two_divisible_by_five
-    assert_equal true, !isDivisibleByFive(2)
+    assert_equal true, !is_divisible_by_five(2)
   end
 
   def test_is_three_divisible_by_five
-    assert_equal true, !isDivisibleByFive(3)
+    assert_equal true, !is_divisible_by_five(3)
   end
 
   def test_is_five_divisible_by_five
-    assert_equal true, isDivisibleByFive(5)
+    assert_equal true, is_divisible_by_five(5)
   end
 
   def test_is_fifteen_divisible_by_five
-    assert_equal true, isDivisibleByFive(15)
+    assert_equal true, is_divisible_by_five(15)
   end
 
   def test_is_square_of_five_divisible_by_five
-    assert_equal true, isDivisibleByFive(25)
+    assert_equal true, is_divisible_by_five(25)
   end
 end
 
 
 class TestSumOfMultiples < MiniTest::Test
   def test_sum_of_all_multiples_of_3_or_5_below_10_euler_example
-    assert_equal 23, sumOfMultiples(10)
+    assert_equal 23, sum_of_multiples(10)
   end
 
   def test_sum_of_all_multiples_of_3_or_5_below_1000_euler_challenge
-    assert_equal 233168, sumOfMultiples(1000)
+    assert_equal 233168, sum_of_multiples(1000)
   end
 end
 
