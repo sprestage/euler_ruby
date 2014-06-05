12aad4fcfaaddd0976582f8e955c2bba35683c3a
Susan Prestage <susan.prestage@gmail.com>
2014-06-04 20:30:15 -0700

    Refactor to put everything in methods where it should be.

--- largest_palindrome_product_euler_04.rb
+++ largest_palindrome_product_euler_04.rb
@@ -8,13 +8,6 @@
 # Find the largest palindrome made from the product of two 3-digit numbers.
 ###
 
-smallest_three_digit = 100
-largest_three_digit = 999
-
-largest_palindrome_product = 0
-first_digit_from_largest_product = 0
-second_digit_from_largest_product = 0
-
 def is_palindrome(number)
   palindrome_string = number.to_s
   palindrome_array = palindrome_string.split("")
@@ -36,20 +29,33 @@ def is_palindrome(number)
   return still_palindrome
 end
 
-largest_three_digit.downto(smallest_three_digit) do |x|
-  x.downto(smallest_three_digit) do |y|
-    product = x*y
-    if is_palindrome(product) == true
-      if product > largest_palindrome_product
-        largest_palindrome_product = product
-        first_digit_from_largest_product = x
-        second_digit_from_largest_product = y
+def find_largest_palindrome_made_from_product_of_two_X_digit_nums(x_digits)
+  smallest_x_digit = 10 ** (x_digits - 1)
+  largest_x_digit = (10 ** x_digits) - 1
+
+  largest_palindrome_product = 0
+  first_digit_from_largest_product = 0
+  second_digit_from_largest_product = 0
+
+  largest_x_digit.downto(smallest_x_digit) do |x|
+    x.downto(smallest_x_digit) do |y|
+      product = x*y
+      if is_palindrome(product) == true
+        if product > largest_palindrome_product
+          largest_palindrome_product = product
+          first_digit_from_largest_product = x
+          second_digit_from_largest_product = y
+        end
       end
     end
   end
+
+  puts
+  puts "The largest palindrome made from the product of two 3-digit numbers"
+  puts "is:  #{largest_palindrome_product} = #{first_digit_from_largest_product} * #{second_digit_from_largest_product}."
+  return largest_palindrome_product
 end
 
-puts
-puts "The largest palindrome made from the product of two 3-digit numbers"
-puts "is:  #{largest_palindrome_product} = #{first_digit_from_largest_product} * #{second_digit_from_largest_product}."
+find_largest_palindrome_made_from_product_of_two_X_digit_nums(3)
+
 
