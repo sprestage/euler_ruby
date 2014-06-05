1663e9a14578e314a6baa3ae52d62ef8202fbbbc
Susan Prestage <susan.prestage@gmail.com>
2014-06-05 12:59:38 -0700

    Refactor to put everything in methods where it should be.

--- smallest_multiple_euler_05.rb
+++ smallest_multiple_euler_05.rb
@@ -1,4 +1,12 @@
 #!/usr/bin/env ruby
+###
+# Smallest multiple
+# Problem 5
+###
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+###
 
 def find_power (number, power)
   @result = 1
@@ -33,34 +41,41 @@ def find_factor_from_hash(primes_hash, max)
   return @multiple
 end
 
+def find_smallest_integer_evenly_divisible_by_1_to_max(max_divisor)
 
-smallest_multiple = 1
-small_divisor = 2
-large_divisor = 20
+  smallest_multiple = 1
+  min_divisor = 2
+  # large_divisor = 20
 
-primes = Hash.new(0)
+  primes = Hash.new(0)
 
-# Store all prime factors into a hash.  Key = prime; value = power
-small_divisor.upto(large_divisor) do |x|
-  if is_prime(x)
-    primes[x] = 1
-  else
-    2.upto(x) do |y|
-      count = 0
-      while (x % y) == 0
-        count = count + 1
-        if count > primes[y]
-          primes[y] = primes[y] + 1
+  # Store all prime factors into a hash.  Key = prime; value = power
+  min_divisor.upto(max_divisor) do |x|
+    if is_prime(x)
+      primes[x] = 1
+    else
+      2.upto(x) do |y|
+        count = 0
+        while (x % y) == 0
+          count = count + 1
+          if count > primes[y]
+            primes[y] = primes[y] + 1
+          end
+          x = x/y
         end
-        x = x/y
       end
     end
   end
+  smallest_multiple = find_factor_from_hash(primes, max_divisor)
+  return smallest_multiple
 end
 
 # Use the stored hash of prime factors and their powers to calculate the multiple.
-smallest_multiple = find_factor_from_hash(primes, large_divisor)
 
-puts smallest_multiple
+print "The smallest positive number that is evenly divisible by all of the numbers from 1 to 10 is: "
+puts find_smallest_integer_evenly_divisible_by_1_to_max(10)
+
+print "The smallest positive number that is evenly divisible by all of the numbers from 1 to 20 is: "
+puts find_smallest_integer_evenly_divisible_by_1_to_max(20)
 
 
