8517f48ff4515be27296beb95d1425552d652049
Susan Prestage <susan.prestage@gmail.com>
2014-06-09 10:56:46 -0700

    Refactor to put everything in methods where it should be.

--- sum_square_difference_euler_06.rb
+++ sum_square_difference_euler_06.rb
@@ -1,18 +1,37 @@
 #!/usr/bin/env ruby
+###
+# Sum square difference
+# Problem 6
+###
+# The sum of the squares of the first ten natural numbers is,
 
-sum_of_squares = 0
-square = 0
-sum = 0
-square_of_sums = 0
+# 12 + 22 + ... + 102 = 385
+# The square of the sum of the first ten natural numbers is,
 
-1.upto(100) do |x|
-  square = (x)**2
-  sum_of_squares = sum_of_squares + square
-  sum = sum + x
+# (1 + 2 + ... + 10)2 = 552 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+###
+
+FIRST_X = 10
+
+def find_dif_betw_square_of_sums_and_sum_of_squares(max)
+  sum_of_squares = 0
+  square = 0
+  sum = 0
+  square_of_sums = 0
+
+  1.upto(max) do |x|
+    square = (x)**2
+    sum_of_squares = sum_of_squares + square
+    sum = sum + x
+  end
+
+  square_of_sums = (sum)**2
+
+  difference = square_of_sums - sum_of_squares
+  return difference
 end
 
-square_of_sums = (sum)**2
-
-difference = square_of_sums - sum_of_squares
-
-puts "difference = #{difference}"
+puts "difference = #{find_dif_betw_square_of_sums_and_sum_of_squares(FIRST_X)}"
