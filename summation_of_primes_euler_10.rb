bd8d8a50d6f221e2bfc391643e30ced073f06bf0
Susan Prestage <susan.prestage@gmail.com>
2014-06-23 18:51:42 -0700

    Refactor to put everything into methods.

--- summation_of_primes_euler_10.rb
+++ summation_of_primes_euler_10.rb
@@ -7,65 +7,26 @@
 #
 # Find the sum of all the primes below two million.
 ###
+# include implementations of is_prime, set_up_seive_of_eratosthenes, count_primes, & find_prime
+require_relative './10001st_prime_euler_07.rb'
 
-def is_prime (number)
-  if number == 1
-    return true
-  end
-  2.upto(number) do |x|
-    if (number % x == 0) && (number != x)
-      return false
-    end
-  end
-  return true
-end
-
-# Find first 10_001 primes.
-goal = 10_001
-# Try all integers up to 200_000.
 target = 2_000_000
-# Sum of primes.
-sum = 0
 
-###
-# Set up a hash to use to find primes
-# using the Sieve of Eratosthenes.
-###
-primes = Hash.new
-root_target = Math.sqrt(target).to_i
-final = 0
-# Zero out the hash values.
-primes[1] = 0
-primes[2] = 1
-4.step(target,2) do |x|
-  primes[x] = 0
-end
-# Store ones for every prime value.
-3.step(target,2) do |x|
-  primes[x] = 1
-end
-# Store zeros for the multiples of the prime values.
-3.step(root_target,2) do |x|
-  if is_prime(x)
-    max = target/x
-    2.upto(max) do |y|
-      primes[x*y] = 0
+def sum_of_primes(size)
+  @sum = 0
+  @primes = Hash.new(0)
+  @primes = set_up_sieve_of_eratosthenes(@primes, size)
+
+  1.upto(size) do |x|
+    if @primes[x] == 1
+      @sum = @sum + x
     end
   end
+  return @sum
 end
-###
-# Sieve of Eratosthenes
-# hash of primes now available.
-###
 
-# Add up the primes for output.
-1.upto(target) do |x|
-  if primes[x] == 1
-    sum = sum + x
-  end
-end
 puts
-puts "sum of primes less than #{target}: #{sum}"
+puts "sum of primes less than #{target}: #{sum_of_primes(target)}"
 
 
 
